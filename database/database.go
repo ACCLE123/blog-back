b3f29551975d076c76c2b9e99b8b7bc98f7ea6e6
accle <3196385390@qq.com>
2025-07-11 23:01:40 +0800

    fix: password

--- database/database.go
+++ database/database.go
@@ -9,7 +9,9 @@ import (
 var DB *gorm.DB
 
 func Connect() *gorm.DB {
-	dsn := "host=localhost dbname=db_blog port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	//dsn := "host=localhost dbname=db_blog port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	// 使用 postgres 用户而非 root
+	dsn := "host=localhost user=root dbname=db_blog password=123456 port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
